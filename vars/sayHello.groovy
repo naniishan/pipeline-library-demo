a3a678700841e9e066a64de8ceaa1ee5df981dff
naniishan <ishan.waviz@gmail.com>
2019-01-23 19:03:42 +0530

    fsdfsf

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,33 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(){
-    node {
-        stage('Checkout') {
-            checkout scm
-        }
-
-        // Execute different stages depending on the job
-        if(env.JOB_NAME.contains("deploy")){
-            packageArtifact()
-        } else if(env.JOB_NAME.contains("test")) {
-            buildAndTest()
-        }
-    }
-}
-
-def packageArtifact(){
-    stage("Package artifact") {
-        "sh echo 'my name is ishan'"
-    }
-}
-
-def buildAndTest(){
-    stage("Backend tests"){
-        "sh echo 'my name is arima'"
-    }
-}
-#!/usr/bin/env groovy
-
 def call(String name = 'human') {
   echo "Hello, ${name}."
 }
