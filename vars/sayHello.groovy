3eca9249ec924ebfe982c82145ca2a1010f66abc
naniishan <ishan.waviz@gmail.com>
2019-01-23 18:28:45 +0530

    fsdff

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -9,3 +9,17 @@ def sample(String name = 'human') {
   echo "my name is ${name}"
 }
 
+
+def logs(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout the file') {
+                steps {
+                    sh 'aws s3 cp s3://logs-s3-to-local /home/ubuntu/'
+                }
+            }
+        }
+    }
+}
