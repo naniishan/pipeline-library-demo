80d10009f886331b4c4c1b4c88b56806af4820f8
naniishan <ishan.waviz@gmail.com>
2019-01-23 18:37:58 +0530

    sdfsdf

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,23 +1,13 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
-}
-
-
-def sample(String name = 'human') {
-  echo "my name is ${name}"
-}
-
-
-def logs(Map pipelineParams) {
+def call(Map pipelineParams) {
 
     pipeline {
         agent any
         stages {
             stage('checkout the file') {
                 steps {
-                    sh 'aws s3 cp s3://logs-s3-to-local /home/ubuntu/'
+                    sh 'aws s3 cp s3://logs-s3-to-local pipelineParams.path'
                 }
             }
         }
